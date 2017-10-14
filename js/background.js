6e2ec48a3e8ccb69b01afafcb881d321fb6cd803
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2017-10-14 01:51:15 +0100

    Updated theme
    - Added fallback for when backdrop-filter is not supported
    - Increased the padding on footer
    - Added support for prefers-reduced-motion

--- js/background.js
+++ js/background.js
@@ -1,6 +1,10 @@
+var prefersReducedMotion = window.matchMedia("(prefers-reduced-motion)");
 $(document).on("scroll", function() {
 	var scrolltop = -$(document).scrollTop(),
 		offset = scrolltop / 5;
+	
+	if(prefersReducedMotion.matches)
+		return;
 	
 	$(".background").css({
 		"background-position": "center " + offset + "px"
--- js/fadein.js
+++ js/fadein.js
@@ -1,6 +1,8 @@
 $(".fade-in-on-scroll").css("visibility", "hidden").css("opacity", 0);
 $(".fade-out-on-scroll").css("display", "block").css("opacity", 1);
 
+var prefersReducedMotion = window.matchMedia("(prefers-reduced-motion)");
+
 $.fadein = {};
 
 $(document).on("scroll", $.fadein.refresh = function() {
@@ -9,29 +11,36 @@ $(document).on("scroll", $.fadein.refresh = function() {
 		windowheight = $(window).height() - headerheight,
 		documentheight = $(document).height() - headerheight,
 		percentage = scrolltop / (documentheight - windowheight),
-		centertop = documentheight * percentage;
+		centertop = documentheight * percentage,
+		scrollbottom = scrolltop + windowheight;
 	
 	$(".fade-in-on-scroll").each(function() {
 		var $this = $(this),
 			offset = $this.offset().top,
+			offsetbottom = offset + $this.outerHeight(),
 			$mtop, $mbottom;
 		
 		if(centertop < offset)
 			return;
 		
-		$mtop = $("<div></div>").css("padding-top", 100).animate({
-			"padding-top": 0
-		}, 800, function() {
-			$(this).remove();
-			$this.data("fadein-top", null);
-		});
-		
-		$mbottom = $("<div></div>").css("margin-top", -100).animate({
-			"margin-top": 0
-		}, 800, function() {
-			$(this).remove();
-			$this.data("fadein-bottom", null);
-		});
+		if(prefersReducedMotion.matches) {
+			$mtop = $("<div></div>");
+			$mbottom = $("<div></div>");
+		} else {
+			$mtop = $("<div></div>").css("padding-top", 100).animate({
+				"padding-top": 0
+			}, 800, function() {
+				$(this).remove();
+				$this.data("fadein-top", null);
+			});
+			
+			$mbottom = $("<div></div>").css("margin-top", -100).animate({
+				"margin-top": 0
+			}, 800, function() {
+				$(this).remove();
+				$this.data("fadein-bottom", null);
+			});
+		}
 			
 		$this.removeClass("fade-in-on-scroll").addClass("done-fade-in-on-scroll").addClass("fade-in-on-scroll-animate").data({
 			"fadein-top": $mtop,
@@ -45,16 +54,43 @@ $(document).on("scroll", $.fadein.refresh = function() {
 		});
 	});
 	
+	$(".done-fade-in-on-scroll").each(function() {
+		var $this = $(this),
+			offset = $this.offset().top,
+			offsetbottom = offset + $this.outerHeight(),
+			$mtop, $mbottom;
+		
+		if(offset < scrollbottom)
+			return;
+		
+		$this.stop().addClass("fade-in-on-scroll").removeClass("done-fade-in-on-scroll").removeClass("fade-in-on-scroll-animate").css("visibility", "hidden").css("opacity", 0);
+		if(($mtop = $($this.data("fadein-top"))).length > 0) $mtop.stop().remove();
+		if(($mbottom = $($this.data("fadein-bottom"))).length > 0) $mbottom.stop().remove();
+		$this.data({ "fadein-top": null, "fadein-bottom": null });
+	});
+	
 	$(".fade-out-on-scroll").each(function() {
 		var $this = $(this),
 			offset = $this.offset().top;
 		
-		if(centertop >= offset)
-			$this.removeClass("fade-out-on-scroll").addClass("done-fade-out-on-scroll").addClass("fade-out-on-scroll-animate").slideUp(800).animate({
-				"opacity": 0
-			}, 800, function() {
-				$this.removeClass("fade-out-on-scroll-animate");
-			}).slideUp(800);
+		if(centertop < offset)
+			return;
+		
+		$this.removeClass("fade-out-on-scroll").addClass("done-fade-out-on-scroll").addClass("fade-out-on-scroll-animate").slideUp(800).animate({
+			"opacity": 0
+		}, 800, function() {
+			$this.removeClass("fade-out-on-scroll-animate");
+		}).slideUp(800);
+	});
+	
+	$(".done-fade-out-on-scroll").each(function() {
+		var $this = $(this),
+			offset = $this.offset().top;
+		
+		if(offset < scrollbottom)
+			return;
+		
+		$(this).stop().removeClass("done-fade-out-on-scroll").removeClass("fade-out-on-scroll-animate").addClass("fade-out-on-scroll").css("display", "block").css("opacity", 1);
 	});
 });
 
